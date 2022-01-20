56a2b71bf4d6cd42aa4f997de8af16d842d24908
Hung, Johnny <johnny.hung@mckesson.com>
2022-01-20 15:48:43 -0800

    fixed and updated the trnasitions

--- src/components/Menu-Item/MenuItem.js
+++ src/components/Menu-Item/MenuItem.js
@@ -5,13 +5,16 @@ const { Title } = Typography;
 
 const MenuItem = ({ imageUrl, title, size}) => {
   return (
-    <div className={`menu-item ${!!size ? size : ""}`} style={{
-      backgroundImage: `url(${imageUrl})`
-    }}>
-      <div className="content">
-        <Title className="title">{title}</Title>
-        <Button className="subtitle" size="default">Purchase</Button>
+    <div className={`menu-item ${!!size ? size : ""}`}>
+      <div className='background-img'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}>
       </div>
+        <div className="content">
+          <Title className="title">{title.toUpperCase()}</Title>
+          <Button className="subtitle" size="default">Purchase</Button>
+        </div>
     </div>
   );
 };
